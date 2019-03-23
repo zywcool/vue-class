8e114217d476c5815d04b51aac2a269cfbfa32de
zywcool <zywcool12@hotmail.com>
2019-03-23 12:10:53 +0800

    2019-03-23 上午

---
+++ my-vue/src/components/BottomBar.vue
@@ -0,0 +1,71 @@
+<template>
+	
+	<div class="bottom-bar flex">
+		
+		<div class="icon-btn">
+			<i class="fa fa-shopping-cart"></i>
+			<div class="title">购物车</div>
+		</div>
+		<div class="icon-btn">
+			<i class="fa fa-shopping-basket"></i>
+			<div class="title">进店</div>
+		</div>
+		
+		<div class="flex flex-item">
+			
+			<div class="btn flex-item orange"><span>加入购物车</span></div>
+			<div class="btn flex-item"><span>立即购买</span></div>
+			
+		</div>
+		
+	</div>
+	
+</template>
+
+<script>
+</script>
+
+<style>
+	.bottom-bar {
+		position: fixed;
+	    bottom: 0;
+	    left: 0;
+	    right: 0;
+	    height: 1rem;
+	    margin: 0 auto;
+	    background-color: #fff;
+	    color: #999;
+	    font-size: 0.24rem;
+	}
+	
+	
+	.btn{
+		position: relative;
+	    z-index: 10;
+	    line-height: 1rem;
+	    text-align: center;
+	    background: #e4393c;
+	    color: #fff;
+	    font-size: .28rem;
+	    cursor: pointer;
+	    -webkit-user-select: none;
+	}
+	
+	.btn.orange {
+		background: #ff8c42;
+	}
+	
+	.bottom-bar .icon-btn{
+		text-align: center;
+		width: 14%;
+		padding-top: 0.15rem;
+	}
+	
+	.bottom-bar .icon-btn i{
+		font-size: 0.4rem;
+	}
+	
+	.bottom-bar .icon-btn .title{
+		line-height: 0.4rem;
+	}
+</style>
---
+++ my-vue/src/components/ProductSku.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="sku mask" @click="show=!show;" >
+		
+		<transition name="slide">
+			<div class="main" v-show="show" @click.stop>
+				<div class="header"></div>
+				<div class="body"></div>
+			</div>
+		</transition>
+	</div>
+</template>
+
+<script>
+	
+	export default{
+		data(){
+			return {
+				show:true
+			};
+		}
+	}
+	
+	
+</script>
+
+<style>
+	
+	.mask{
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-color: rgba(0,0,0,.7);
+		z-index: 999999;
+	}
+	
+	.mask .main{
+		width: 100%;
+		height: 9.2rem;
+		background-color: #FFFFFF;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+	}
+	
+	.sku .slide-enter{
+		bottom: -9.2rem;
+	}
+	.sku .slide-enter-active{
+		transition: bottom 0.3s;
+	}
+	.sku .slide-enter-to{
+		bottom: 0;
+	}
+	.sku .slide-leave{}
+	.sku .slide-leave-active{}
+	.sku .slide-leave-to{}
+	
+</style>
